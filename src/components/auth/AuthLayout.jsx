acc187581caa4db52d8f03354ca919a29edecaa3
qp4534 <nh01240@naver.com>
2026-08-02 00:04:17 +0900

    jsx는 이제 그만. css만 계속 수정중: 관리 관제 따라서 로고에 번개 아이콘 수정함

--- src/components/auth/AuthLayout.jsx
+++ src/components/auth/AuthLayout.jsx
@@ -18,6 +18,7 @@ export default function AuthLayout({
         </header>
       ) : (
         <div className="auth-page-logo auth-page-logo--plain">
+          <FiZap className="auth-page-logo-icon auth-page-logo-icon--plain" />
           <span>MijungE</span>
         </div>
       )}
