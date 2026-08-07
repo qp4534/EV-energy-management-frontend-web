faa095da40c0901602dc4641a90e30fc5e3a6a56
EUNCHEOK <kjh82701@naver.com>
2026-08-08 00:57:31 +0900

    차량 목록 페이지에서 디지털 트윈 카드 제거

--- src/pages/Controller/CarList.jsx
+++ src/pages/Controller/CarList.jsx
@@ -3,7 +3,6 @@ import StatCardList from "@/components/controller/StatCardList";
 import SearchFilterBar from "@/components/common/SearchFilterBar";
 import CarFilterPanel from "@/components/controller/carList/CarFilterPanel";
 import CarVehicleList from "@/components/controller/carList/CarVehicleList";
-import CarDigitalTwinCard from "@/components/controller/carDetail/CarDigitalTwinCard";
 import Pagination from "@/components/common/Pagination";
 import { useCarTableList, useStopCharging } from "@/hooks/queries/useCar";
 import { DEFAULT_CAR_FILTERS, PAGE_SIZE } from "@/constants/carList.constants";
@@ -34,7 +33,6 @@ export default function CarList() {
   const stopCharging = (carId) => {
     stopChargingMutation.mutate(carId);
   };
-  const firstCarId = data?.items?.[0]?.carId;
 
   return (
     <div className="flex flex-col gap-4 p-6">
@@ -75,12 +73,6 @@ export default function CarList() {
         />
       </div>
 
-      {firstCarId && (
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-          <CarDigitalTwinCard mode="live" vehicleId={firstCarId} />
-          <CarDigitalTwinCard mode="history" vehicleId={firstCarId} />
-        </div>
-      )}
     </div>
   );
 }
